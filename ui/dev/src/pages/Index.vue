9545150f3f231b7729d031bb1d5eb1b3a07c45ca
Christian Behon <christian.behon@knusperleicht.at>
2021-07-02 14:22:08 +0200

    chore(dev): refactor  to composition api

--- ui/dev/src/pages/Index.vue
+++ ui/dev/src/pages/Index.vue
@@ -1,11 +1,20 @@
 <template>
   <q-page class="row items-center justify-evenly">
 
-    <q-overlay :no-scroll="data.noScroll" :id="'component'" v-model="component" :background-color="data.whiteBackground ? '#FFF' : '#000'">
+    <q-overlay v-model="fullscreen" :id="'full'" :background-color="backgroundColor">
+      <template v-slot:body>
+        <div class="fullscreen row justify-center items-center">
+          <q-spinner v-if="fullscreen && waiting" color="yellow" size="3em"></q-spinner>
+          <q-btn v-if="fullscreen && !waiting" color="primary" label="Exit" @click="onClickFullscreen"/>
+        </div>
+      </template>
+    </q-overlay>
+
+    <q-overlay :no-scroll="noScroll" :id="'component'" v-model="component" :background-color="backgroundColor">
       <template v-slot:body>
         <div class="absolute fit row justify-center items-center">
-          <q-spinner v-if="component && data.waiting" color="yellow" size="3em"></q-spinner>
-          <q-btn v-if="component && !data.waiting" color="primary" label="Exit" @click="component = !component"/>
+          <q-spinner v-if="component && waiting" color="yellow" size="3em"></q-spinner>
+          <q-btn v-if="component && !waiting" color="primary" label="Exit" @click="onClickComponent"/>
         </div>
       </template>
       <q-card class="my-card bg-secondary text-white">
@@ -27,80 +36,108 @@
         </q-card-section>
 
         <q-separator dark/>
-        <q-checkbox v-model="data.noScroll" dark label="No scroll"/>
-        <q-checkbox v-model="data.whiteBackground" dark label="White Background"/>
+        <q-checkbox v-model="noScroll" dark label="No scroll"/>
+        <q-checkbox v-model="whiteBackground" dark label="White Background"/>
         <q-separator dark/>
 
         <q-card-actions>
-          <q-btn flat @click="fullscreen = !fullscreen">Fullscreen</q-btn>
-          <q-btn flat @click="component = !component">Component</q-btn>
+          <q-btn flat @click="onClickFullscreen">Fullscreen</q-btn>
+          <q-btn flat @click="onClickComponent">Component</q-btn>
         </q-card-actions>
       </q-card>
     </q-overlay>
-    <q-overlay v-model="fullscreen" :id="'full'" :no-scroll="data.noScroll" >
-      <template v-slot:body>
-        <div class="fullscreen row justify-center items-center">
-          <q-spinner v-if="fullscreen && data.waiting" color="yellow" size="3em"></q-spinner>
-          <q-btn v-if="fullscreen && !data.waiting" color="primary" label="Exit" @click="fullscreen = !fullscreen"/>
-        </div>
-      </template>
-    </q-overlay>
+
   </q-page>
 </template>
 
 <script lang="ts">
 
-import { defineComponent, reactive, ref, UnwrapRef, watch } from 'vue'
+import { defineComponent, reactive, ref, UnwrapRef, toRefs, computed } from 'vue'
 
-interface UIModel {
+const millisecondsToWait = 2000
+
+interface QOverlayProps {
   noScroll: boolean,
   waiting: boolean,
   timerId: any,
   whiteBackground: boolean
 }
 
+function useFullScreenOverlay (props: UnwrapRef<QOverlayProps>) {
+  const fullscreen = ref<boolean>(false)
+  const onClickFullscreen = () => {
+    fullscreen.value = !fullscreen.value
+    clearTimeout(props.timerId)
+    if (fullscreen.value) {
+      props.waiting = true
+      props.timerId = setTimeout(() => {
+        props.waiting = false
+      }, millisecondsToWait)
+    } else {
+      props.waiting = false
+    }
+  }
+
+  return {
+    fullscreen,
+    onClickFullscreen
+  }
+}
+
+function useComponentOverlay (props: UnwrapRef<QOverlayProps>) {
+  const component = ref<boolean>(false)
+
+  const onClickComponent = () => {
+    component.value = !component.value
+    clearTimeout(props.timerId)
+    if (component.value) {
+      props.waiting = true
+      props.timerId = setTimeout(() => {
+        props.waiting = false
+      }, millisecondsToWait)
+    } else {
+      props.waiting = false
+    }
+  }
+
+  return {
+    component,
+    onClickComponent
+  }
+}
+
 export default defineComponent({
-  name: 'PageIndex',
+  name: 'DevExample',
   components: {},
   setup () {
-    const fullscreen = ref(false)
-    const component = ref(false)
-
-    const data: UnwrapRef<UIModel> = reactive({
+    const qprops = reactive<QOverlayProps>({
       noScroll: false,
       waiting: false,
       timerId: null,
       whiteBackground: false
     })
 
-    watch(() => component.value, (val) => {
-      clearTimeout(data.timerId)
-      if (val) {
-        data.waiting = true
-        data.timerId = setTimeout(() => {
-          data.waiting = false
-        }, 5000)
-      } else {
-        data.waiting = false
-      }
-    })
+    const {
+      fullscreen,
+      onClickFullscreen
+    } = useFullScreenOverlay(qprops)
 
-    watch(() => fullscreen.value, (val) => {
-      clearTimeout(data.timerId)
-      if (val) {
-        data.waiting = true
-        data.timerId = setTimeout(() => {
-          data.waiting = false
-        }, 5000)
-      } else {
-        data.waiting = false
-      }
+    const {
+      component,
+      onClickComponent
+    } = useComponentOverlay(qprops)
+
+    const backgroundColor = computed(() => {
+      return qprops.whiteBackground ? '#FFF' : '#000'
     })
 
     return {
-      data,
+      ...toRefs(qprops),
       fullscreen,
-      component
+      component,
+      backgroundColor,
+      onClickFullscreen,
+      onClickComponent
     }
   }
 })
