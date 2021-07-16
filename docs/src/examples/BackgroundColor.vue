27001c06c1fd7a76c02f1c5af4a9bfccd5f220b5
Jeff Galbraith <galbraith64@gmail.com>
2021-07-16 07:40:46 -0600

    chore(docs): add background color example

---
+++ docs/src/examples/BackgroundColor.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="row justify-center q-pa-md q-gutter-sm">
+
+    <q-checkbox v-model="noScroll" label="Disable scrolling when showing overlay" />
+    <div class="column">
+      <q-btn label="Show Fullscreen Overlay (Red)" @click="onClickComponentOverlay('rgb(255,0,0)')"></q-btn>
+      <q-btn label="Show Fullscreen Overlay (Green)" @click="onClickComponentOverlay('rgb(0,255,0)')"></q-btn>
+      <q-btn label="Show Fullscreen Overlay (Blue)" @click="onClickComponentOverlay('#0000FF')"></q-btn>
+    </div>
+
+    <q-overlay v-model="show" :background-color="color" :no-scroll="noScroll" :z-index="5000">
+      <template #body>
+        <div class="fullscreen row justify-evenly items-center">
+          <q-spinner v-if="show === true && waiting === true" color="yellow" size="3em"></q-spinner>
+          <q-btn v-if="show === true && waiting !== true" color="primary" label="Exit" @click="onClickComponentOverlay" />
+        </div>
+      </template>
+    </q-overlay>
+  </div>
+
+</template>
+
+<script>
+import { defineComponent, onBeforeUnmount, reactive, toRefs, ref } from 'vue'
+import { QOverlay } from '@quasar/quasar-ui-qoverlay'
+import '@quasar/quasar-ui-qoverlay/src/QOverlay.sass'
+
+function useComponentOverlay(properties, color) {
+
+  onBeforeUnmount(() => {
+    clearTimeout(properties.timerId)
+  })
+
+  const onClickComponentOverlay = (colorName) => {
+    clearTimeout(properties.timerId)
+    properties.show = !properties.show
+    if (properties.show) {
+      console.log('colorName:', colorName)
+      color.value = colorName
+      properties.waiting = true
+      properties.timerId = setTimeout(() => {
+        properties.waiting = false
+        properties.show = false
+      }, 2000)
+    }
+    else {
+      properties.waiting = false
+    }
+  }
+
+  return {
+    onClickComponentOverlay
+  }
+}
+
+export default defineComponent({
+  name: 'BackgroundColor',
+  components: {
+    QOverlay
+  },
+
+  setup () {
+    const properties = reactive({
+      show: false,
+      noScroll: false,
+      waiting: false,
+      timerId: null
+    })
+    const color = ref('rgb(255,0,0)')
+
+    const {
+      onClickComponentOverlay
+    } = useComponentOverlay(properties, color)
+
+    return {
+      ...toRefs(properties),
+      color,
+      onClickComponentOverlay
+    }
+  }
+})
+</script>
--- docs/src/pages/developing/using-qoverlay.md
+++ docs/src/pages/developing/using-qoverlay.md
@@ -29,3 +29,13 @@ title=""
 file="ButtonWithMenu"
 codepen-title="QOverlay"
 />
+
+### Background Color
+
+When setting the background color you **must** use **rgb** or **#000000** formats (example: `rgb(255,0,0)` or `#0000FF` will give you red). Using a CSS color name or one from the Quasar Color Palette will not work.
+
+<example-viewer
+title=""
+file="BackgroundColor"
+codepen-title="QOverlay"
+/>
